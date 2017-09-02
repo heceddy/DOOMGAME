608ac8e8db07177bcfe3c827d68533fb868c6f7d
heceddy <edward.knowles06@gmail.com>
2017-09-02 16:22:58 +0100

    Add files via upload

--- MAIN.py
+++ MAIN.py
@@ -9,7 +9,7 @@ RED = (255, 0, 0)
 pygame.init()
 
 # Set the width and height of the screen [width, height]
-size = (1280, 720)
+size = (700, 500)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("My Game")
@@ -20,6 +20,7 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 background_image = pygame.image.load("doom4_shots.jpg").convert()
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -36,10 +37,9 @@ while not done:
 
     # If you want a background image, replace this clear with blit'ing the
     # background image.
-    screen.blit(background_image, [0, 0])
+    screen.fill(WHITE)
 
     # --- Drawing code should go here
-    pygame.draw.rect(screen, WHITE, [0, 600, 1280, 40], 0)
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
